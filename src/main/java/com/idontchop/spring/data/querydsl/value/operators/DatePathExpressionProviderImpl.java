9bbf57483233377f860c3b8057d1c02d1d13118f
idontchop <nate@idontchop.com>
2025-03-11 12:43:01 -0700

    0.2.0

--- src/main/java/com/idontchop/spring/data/querydsl/value/operators/DatePathExpressionProviderImpl.java
+++ src/main/java/com/idontchop/spring/data/querydsl/value/operators/DatePathExpressionProviderImpl.java
@@ -1,94 +1,95 @@
 package com.idontchop.spring.data.querydsl.value.operators;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.DateTimePath;
+import com.querydsl.core.types.dsl.DatePath;
+import com.querydsl.core.types.dsl.DatePath;
 import org.apache.commons.lang3.Validate;
 
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
 
-class DateTimePathExpressionProviderImpl extends BaseExpressionProvider<DateTimePath> {
+class DatePathExpressionProviderImpl extends BaseExpressionProvider<DatePath> {
 
-    public DateTimePathExpressionProviderImpl() {
+    public DatePathExpressionProviderImpl() {
         super(List.of(Operator.EQUAL, Operator.NOT_EQUAL, Operator.GREATER_THAN, Operator.GREATER_THAN_OR_EQUAL,
                 Operator.LESS_THAN, Operator.LESS_THAN_OR_EQUAL));
     }
 
     @Override
-    protected String getStringValue(DateTimePath path, Object value) {
+    protected String getStringValue(DatePath path, Object value) {
         return value.toString();
     }
 
     @Override
-    protected BooleanExpression eq(DateTimePath path, String value, boolean ignoreCase) {
+    protected BooleanExpression eq(DatePath path, String value, boolean ignoreCase) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.eq(convertToDate(value));
     }
 
     @Override
-    protected BooleanExpression ne(DateTimePath path, String value, boolean ignoreCase) {
+    protected BooleanExpression ne(DatePath path, String value, boolean ignoreCase) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.ne(convertToDate(value));
     }
 
     @Override
-    protected BooleanExpression contains(DateTimePath path, String value, boolean ignoreCase) {
+    protected BooleanExpression contains(DatePath path, String value, boolean ignoreCase) {
         throw new UnsupportedOperationException("Datetime can't be searched using contains operator");
     }
 
     @Override
-    protected BooleanExpression startsWith(DateTimePath path, String value, boolean ignoreCase) {
+    protected BooleanExpression startsWith(DatePath path, String value, boolean ignoreCase) {
         throw new UnsupportedOperationException("Datetime can't be searched using startsWith operator");
     }
 
     @Override
-    protected BooleanExpression endsWith(DateTimePath path, String value, boolean ignoreCase) {
+    protected BooleanExpression endsWith(DatePath path, String value, boolean ignoreCase) {
         throw new UnsupportedOperationException("Datetime can't be searched using endsWith operator");
     }
 
     @Override
-    protected BooleanExpression matches(DateTimePath path, String value) {
+    protected BooleanExpression matches(DatePath path, String value) {
         throw new UnsupportedOperationException("Datetime can't be searched using matches operator");
     }
 
     @Override
-    protected BooleanExpression gt(DateTimePath path, String value) {
+    protected BooleanExpression gt(DatePath path, String value) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.gt(convertToDate(value));
     }
 
     @Override
-    protected BooleanExpression gte(DateTimePath path, String value) {
+    protected BooleanExpression gte(DatePath path, String value) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.goe(convertToDate(value));
     }
 
     @Override
-    protected BooleanExpression lt(DateTimePath path, String value) {
+    protected BooleanExpression lt(DatePath path, String value) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.lt(convertToDate(value));
     }
 
     @Override
-    protected BooleanExpression lte(DateTimePath path, String value) {
+    protected BooleanExpression lte(DatePath path, String value) {
         Validate.isTrue(isDate(value), "Invalid date value");
         return path.loe(convertToDate(value));
     }
 
     private boolean isDate(String dateString) {
         try {
-            System.out.println("\n\n\n" + dateString);
-            ZonedDateTime.parse(dateString, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+            LocalDate.parse(dateString);
             return true;
         } catch (DateTimeParseException iae) {
             return false;
         }
     }
 
-    private ZonedDateTime convertToDate(String dateString) {
+    private LocalDate convertToDate(String dateString) {
         // use the same conversion as used by the Conversion service
-        return ZonedDateTime.parse(dateString, DateTimeFormatter.ISO_ZONED_DATE_TIME);
+        return LocalDate.parse(dateString);
     }
 }
